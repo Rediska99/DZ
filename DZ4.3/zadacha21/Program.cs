bc371a489c8f32abca714ffafea30a2163c790ab
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-08-19 01:34:22 +0300

    Add files via upload

---
+++ DZ4.3/zadacha21/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a_x = 1;
+            int a_y = 1;
+            int a_z = 1;
+            int b_x = 0;
+            int b_y = 0;
+            int b_z = 0;
+            double distance = Math.Sqrt(Math.Pow(a_x - b_x,2) + Math.Pow(a_y - b_y,2) + Math.Pow(a_z - b_z,2));
+            Console.WriteLine(distance);  
+
+        }    
+    }    
---
+++ DZ4.3/zadacha23/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          int number=9;
+          for(int i = 1; i <= 1; i++)
+          {
+            Console.WriteLine($"{i} * {i} * {i} = {Math.Pow(i, 3)}");
+          }
+        }    
+    }    
