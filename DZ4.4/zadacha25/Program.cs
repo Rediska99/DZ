e66dee5d6ac0401df1b277cefdfacfc7c378b905
Rediska99 <106888993+Rediska99@users.noreply.github.com>
2022-08-22 23:29:48 +0300

    Add files via upload

---
+++ DZ4.4/zadacha25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 2;
+            int result = a;
+            for(int i=1; i < b; i++)
+            {
+                result *= a;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
